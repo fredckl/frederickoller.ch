1a867b06dda3a1bd930878cd7c30d3ac9aa9ef01
Frederic KOLLER <koller.f@gmail.com>
2018-10-29 08:03:31 +0100

    Amélioration des effets éclaires

--- nuxt.config.js
+++ nuxt.config.js
@@ -23,7 +23,10 @@ module.exports = {
       { rel: 'icon', type: 'image/x-icon', href: '/favicon.ico' },
       { rel: 'stylesheet', href: 'https://fonts.googleapis.com/css?family=Amaranth:400,700'},
       { rel: 'stylesheet', href: 'https://use.fontawesome.com/releases/v5.4.2/css/all.css', integrity: 'sha384-/rXc/GQVaYpyDdyxK+ecHPVYJSN9bmVFBvjA/9eOB+pb3F2w2N6fc5qB9Ew5yIns', crossorigin: 'anonymous'}
-    ]
+    ],
+    htmlAttrs: {
+      lang: 'fr',
+    }
   },
 
   /*
